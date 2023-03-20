e058160b1a040efbb5f988698c9652c681ad3f09
ivan <ivansyn2013@yandex.ru>
2023-03-20 14:44:49 +0300

    start admin dz

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,46 @@
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from blog.models.init_db import db
 from blog import models
+from flask_login import current_user
+from flask import redirect, url_for
 
+
+class MyAdminIndexView(AdminIndexView):
+    @expose('/')
+    def index(self):
+        if not (current_user.is_authenticated and current_user.is_staff):
+            return redirect(url_for("auth_app.login"))
+        return super(MyAdminIndexView, self).index()
 class CustomAdminViews(ModelView):
-    pass
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('auth_app.login'))
+
+class TagAdminView(CustomAdminViews):
+    column_searchable_list = ("name",)
+    column_filters = ("name",)
+    can_export = True
+    export_types = ["csv", "xlsx"]
+    create_modal = True
+    edit_modal = True
+
+class UserAdminView(CustomAdminViews):
+    column_exclude_list = ("_password",)
+    column_searchable_list = ("first_name", "last_name", "username", "is_staff", "email")
+    column_filters = ("first_name", "last_name", "username", "is_staff", "email")
+    column_editable_list = ("first_name", "last_name", "is_staff")
+    can_create = True
+    can_edit = True
+    can_delete = False
 
 
-admin = Admin(name='Blog Admin', template_mode='bootstrap4')
-admin.add_view(CustomAdminViews(models.Tag, db.session, category='Models'))
+admin = Admin(name='Blog Admin',
+              index_view=MyAdminIndexView(),
+              template_mode='bootstrap4')
+#admin.add_view(CustomAdminViews(models.Tag, db.session, category='Models'))
+admin.add_view(CustomAdminViews(models.Article, db.session, category="Models"))
+admin.add_view(TagAdminView(models.Tag, db.session, category='Models'))
+admin.add_view(UserAdminView(models.User, db.session, category="Models"))
