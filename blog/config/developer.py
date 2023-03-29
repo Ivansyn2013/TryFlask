3343acfdf710c36c471a9fca8e7a27fe4c222f67
ivan <ivansyn2013@yandex.ru>
2023-03-29 22:18:39 +0300

    finish dz 11 deploy

--- blog/config/developer.py
+++ blog/config/developer.py
@@ -22,13 +22,11 @@ class DevelopmemtConfig(Config):
 class DeployConfig(Config):
     load_dotenv()
     config = dotenv_values('.env')
+    FLASK_DEBUG = False
+    TESTING = False
     SQLALCHEMY_DATABASE_URI = config['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_MODIFICATIONS = config['SQLALCHEMY_MODIFICATIONS']
     SECRET_KEY = config['SECRET_KEY']
-    POSTGRES_DB = config['POSTGRES_DB']
-    POSTGRES_USER = config['POSTGRES_USER']
-    POSTGRES_PASSWORD = config['POSTGRES_PASSWORD']
-    PGDATA = config['PGDATA']
 
 class DeveloperPostgresConfig(Config):
     load_dotenv()
@@ -38,6 +36,7 @@ class DeveloperPostgresConfig(Config):
     SQLALCHEMY_DATABASE_URI = config['SQLALCHEMY_DATABASE_URI']
     SQLALCHEMY_MODIFICATIONS = config['SQLALCHEMY_MODIFICATIONS']
     SECRET_KEY = config['SECRET_KEY']
+
     POSTGRES_DB = config['POSTGRES_DB']
     POSTGRES_USER = config['POSTGRES_USER']
     POSTGRES_PASSWORD = config['POSTGRES_PASSWORD']
